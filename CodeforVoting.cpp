49bb82125bf94cd3ef5b5532bbbbba0b7f86031f
Abby-C10 <abigaeltallam@gmail.com>
2025-02-16 10:51:01 +0300

    Created a new file.
    Contains a code to test the eligibility of a person to vote.

---
+++ CodeforVoting.cpp
@@ -0,0 +1,24 @@
+/*Abigael Tallam
+BSE-05-0067/2024
+16/02/2024
+Zetech University*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string name;
+    int age; 
+    cout<<"Enter your name"<<endl;
+    cin>>name;
+    cout<<"Enter your age"<<endl;
+    cin>>age;
+    if(age>=18){
+        cout<<"Eligible to vote"<<endl;
+
+    }else{
+        cout<<"Cannot vote"<<endl;
+    }
+    return 0;
+    }
